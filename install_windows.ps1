aeb224ef52080a1daf35d3ada9a471331e110e7d
Baipyrus <simon.waltemathe@gmail.com>
2024-08-27 12:32:13 +0200

    initialize windows install script

---
+++ install_windows.ps1
@@ -0,0 +1,5 @@
+# Define paths for tools and configurations
+$dotfilesRepo = "$env:TMP\dotfiles"
+$alacrittyConfigDir = "$env:APPDATA\alacritty"
+$psProfile = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+$repoUrl = "https://github.com/Baipyrus/dotfiles.git"
