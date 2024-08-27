e2797da0b7a6647412cbd5ef7f324a863ad6c4f5
Baipyrus <simon.waltemathe@gmail.com>
2024-08-27 12:33:44 +0200

    cloning or pulling dotfiles repository

--- install_windows.ps1
+++ install_windows.ps1
@@ -3,3 +3,14 @@ $dotfilesRepo = "$env:TMP\dotfiles"
 $alacrittyConfigDir = "$env:APPDATA\alacritty"
 $psProfile = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 $repoUrl = "https://github.com/Baipyrus/dotfiles.git"
+
+# Clone dotfiles repository to TMP if not already inside it
+if (-not (Test-Path $dotfilesRepo))
+{
+    Write-Host "Cloning dotfiles repository..." -ForegroundColor Cyan
+    git clone $repoUrl $dotfilesRepo
+} else
+{
+    Write-Host "Pulling latest changes from dotfiles repository..." -ForegroundColor Cyan
+    git -C $dotfilesRepo pull
+}
