cf885e736373ef5311f640b2d054fe2a54fd7882
Baipyrus <simon.waltemathe@gmail.com>
2024-11-25 11:54:14 +0100

    using wsl install without distro for older versions

--- util/wsl.psm1
+++ util/wsl.psm1
@@ -3,7 +3,7 @@ function InstallWSL
     # Reference: https://learn.microsoft.com/en-us/windows/wsl/install-manual
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    wsl.exe --update
+    wsl.exe --install --no-distribution
     wsl.exe --set-default-version 2
 }
 
