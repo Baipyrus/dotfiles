0da955a90db6205a2e4915e4d713327829278701
Baipyrus <simon.waltemathe@gmail.com>
2024-11-25 11:56:41 +0100

    replace dism.exe for CMD with builtin powershell module

--- util/wsl.psm1
+++ util/wsl.psm1
@@ -1,8 +1,8 @@
 function InstallWSL
 {
     # Reference: https://learn.microsoft.com/en-us/windows/wsl/install-manual
-    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All -NoRestart
     wsl.exe --install --no-distribution
     wsl.exe --set-default-version 2
 }
