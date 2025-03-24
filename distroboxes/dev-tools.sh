951a71c0509cdaca452ee5aec9c367d9d841aff7
Baipyrus <simon.waltemathe@gmail.com>
2025-03-24 17:51:01 +0100

    fix: install neovide using terra repository instead of manual

--- distroboxes/dev-tools.sh
+++ distroboxes/dev-tools.sh
@@ -3,28 +3,14 @@
 # distrobox create -n dev-tools -p -i quay.io/fedora/fedora:41
 # distrobox enter -n dev-tools
 
+# Install Fyra Labs Terra repository
+sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
 # Install programs and sdks
 sudo dnf install -y gcc make git ripgrep fd-find unzip \
-    neovim dotnet-sdk-8.0 golang composer wget \
+    neovide neovim dotnet-sdk-8.0 golang composer wget \
     nodejs-npm alacritty fontconfig libxkbcommon \
     libxkbcommon-x11 libwayland-egl libglvnd-egl
-
-# Install neovide
-sudo mkdir -p /usr/local/bin
-wget -P /tmp/ https://github.com/neovide/neovide/releases/latest/download/neovide-linux-x86_64.tar.gz
-sudo tar -xvzf /tmp/neovide-linux-x86_64.tar.gz -C /usr/bin/
-sudo chmod +x /usr/bin/neovide
-sudo chown root: /usr/bin/neovide
-
-# Install neovide desktop file
-wget -P /tmp/ https://raw.githubusercontent.com/neovide/neovide/main/assets/neovide.desktop
-sudo desktop-file-install /tmp/neovide.desktop
-
-# Install neovide app icon
-sudo mkdir -p /usr/share/pixmaps/
-wget -P /tmp/ https://raw.githubusercontent.com/neovide/neovide/main/assets/neovide.svg
-sudo cp /tmp/neovide.svg /usr/share/pixmaps/
-cp /tmp/neovide.svg ~/.local/share/icons/
 
 # Install configs (nvim/alacritty)
 git clone https://github.com/Baipyrus/nvim-config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
