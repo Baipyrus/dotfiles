cbe1fbcb1c624962f7f2ca8f4f9094bf64894979
Baipyrus <simon.waltemathe@gmail.com>
2025-03-24 21:57:36 +0100

    fix: replace test image with latest base fedora

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v distrobox >/dev/null 2>&1; then
-    distrobox create -n dev-tools -i dev-tools_post-install:latest
+    distrobox create -n dev-tools -i quay.io/fedora/fedora:latest
     distrobox enter -n dev-tools -- bash "./distroboxes/dev-tools.sh"
     distrobox enter -n dev-tools -- bash "./util/linux.sh"
     exit
