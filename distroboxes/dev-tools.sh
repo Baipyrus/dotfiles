096c8fa23e9961346de4c20a6e0e88f173149054
Baipyrus <simon.waltemathe@gmail.com>
2025-03-24 19:29:46 +0100

    feat: install powershell and jq tool in dev-tools container

--- distroboxes/dev-tools.sh
+++ distroboxes/dev-tools.sh
@@ -11,7 +11,15 @@ sudo dnf install -y gcc make git ripgrep fd-find unzip \
     neovide neovim dotnet-sdk-8.0 golang composer wget \
     nodejs-npm alacritty fontconfig libxkbcommon \
     libxkbcommon-x11 libwayland-egl libglvnd-egl \
-    git-credential-oauth
+    git-credential-oauth jq
+
+# Fetch and try installing PowerShell from GitHub
+download_url="https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
+rpm_url=$(curl -s $download_url | \
+    jq -r '.assets[] | select(.name | test(".rh.x86_64.rpm")) | .browser_download_url')
+if [[ -n "$rpm_url" ]]; then
+    sudo dnf install -y "$rpm_url"
+fi
 
 # Clone Neovim config
 CFG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}"
