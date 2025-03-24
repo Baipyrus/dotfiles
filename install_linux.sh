4e80abcc327972fbc749354607481ffb9fab61f2
Baipyrus <simon.waltemathe@gmail.com>
2025-03-24 19:58:13 +0100

    fix: use exit instead of return inside install script

--- install_linux.sh
+++ install_linux.sh
@@ -4,7 +4,7 @@ if command -v distrobox >/dev/null 2>&1; then
     distrobox create -n dev-tools -i dev-tools_post-install:latest
     distrobox enter -n dev-tools -- bash "./distroboxes/dev-tools.sh"
     distrobox enter -n dev-tools -- bash "./util/linux.sh"
-    return
+    exit
 fi
 
 . "./util/linux.sh"
